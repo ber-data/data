5a2f0f4b641537e4e9396f4508aa07d64e0f7c54
eecavanna <eecavanna@users.noreply.github.com>
2025-09-15 11:10:42 -0700

    Add module-level documentation to NMDC ingest script

---
+++ ingest/nmdc/ingest.py
@@ -0,0 +1,11 @@
+"""
+This script can be used to EXTRACT data from the NMDC database (via the NMDC Runtime API [1]),
+TRANSFORM it into a shape that is compliant with the BERtron Schema [2], and write it to a
+file(s) on the filesystem. That file(s)'s contents can then be LOADED into a the BERtron database.
+
+References:
+1. https://api.microbiomedata.org
+2. https://github.com/ber-data/bertron-schema
+"""
+
+pass
